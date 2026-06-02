c704e14f18f1b578195f9d4b7781e6cd4d4f5195
koguchi-e <koguchi.e915@gmail.com>
2026-06-02 14:10:16 +0900

    Googleアカウントでのログイン機能を追加

---
+++ db/migrate/20260602021213_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[8.1]
+  def change
+    create_table :users do |t|
+      t.string :email_address
+      t.string :provider
+      t.string :uid
+      t.string :name
+
+      t.timestamps
+    end
+
+    add_index :users, [:provider, :uid], unique: true
+    add_index :users, :email_address, unique: true
+  end
+end
