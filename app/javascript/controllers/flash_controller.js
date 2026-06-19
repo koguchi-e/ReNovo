0d926207fa74d6aef814bbdcdaae69198174b6b4
koguchi-e <koguchi.e915@gmail.com>
2026-06-19 15:10:37 +0900

    経過時間で徐々に薄くなるように設定

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -3,7 +3,10 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   connect() {
     setTimeout(() => {
+      this.element.classList.add("opacity-0");
+    }, 100);
+    setTimeout(() => {
       this.element.remove();
-    }, 2000);
+    }, 4000);
   }
 }
