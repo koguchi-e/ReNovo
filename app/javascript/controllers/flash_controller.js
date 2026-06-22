07b7bc10f2b635d7eab3ff3165083582f2b1bbac
koguchi-e <koguchi.e915@gmail.com>
2026-06-22 18:58:38 +0900

    通知の透明化を削除

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -3,10 +3,11 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   connect() {
     setTimeout(() => {
-      this.element.classList.add("opacity-0");
-    }, 100);
-    setTimeout(() => {
       this.element.remove();
-    }, 4000);
+    }, 2000);
+  }
+
+  disconnect() {
+    if (this.dismissTimer) clearTimeout(this.dismissTimer);
   }
 }
