ae38162796da325dfb06263da39dd75436931d08
koguchi-e <koguchi.e915@gmail.com>
2026-06-29 11:34:37 +0900

    タスクのシステムテストを追加する

---
+++ spec/system/tasks_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe "Tasks", type: :system do
+  let(:user) { FactoryBot.create(:user) }
+  let!(:situation) { FactoryBot.create(:situation, user: user) }
+  let!(:task) { FactoryBot.create(:task, situation: situation) }
+
+  before do
+    driven_by(:selenium_chrome_headless)
+    sign_in_as(user)
+  end
+
+  scenario "タスクを追加し、一覧に追加される" do
+    visit situation_tasks_path(situation)
+    find('[data-testid="open-task-form-button"]').click
+
+    find('[data-testid="new-task-content-input"]').fill_in with: "新しいタスク"
+    find('[data-testid="create-task-button"]').click
+
+    expect(page).to have_content "新しいタスク"
+  end
+
+  scenario "タスクが空欄だと登録できない" do
+    visit situation_tasks_path(situation)
+    find('[data-testid="open-task-form-button"]').click
+
+    find('[data-testid="new-task-content-input"]').fill_in with: ""
+    find('[data-testid="create-task-button"]').click
+
+    expect(page).to have_content "タスクの追加に失敗しました。"
+  end
+
+  scenario "タスクを更新し、一覧が変更される" do
+    visit situation_tasks_path(situation)
+    find('[data-testid="edit-task-button"]').click
+
+    find('[data-testid="edit-task-content-input"]').fill_in with: "更新済タスク"
+    find('[data-testid="update-task-button"]').click
+
+    expect(page).to have_content "更新済タスク"
+  end
+
+  scenario "タスクを削除し、一覧から消える" do
+    visit situation_tasks_path(situation)
+
+    accept_confirm "本当に削除しますか？" do
+      find('[data-testid="delete-task-button"]').click
+    end
+
+    expect(page).not_to have_content "明日以降のスケジュールを書き、空き時間を確保する"
+  end
+end
