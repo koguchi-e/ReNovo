fd2a4757bf325aacdeeec63b86593789b774d451
koguchi-e <koguchi.e915@gmail.com>
2026-06-29 09:45:35 +0900

    タスクのモデルテストを追加

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,4 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
+  describe "バリデーション" do
+    it "タスクを入力して登録できる" do
+      task = build(:task)
+      expect(task).to be_valid
+    end
+    it "タスクが空欄だと登録できない" do
+      task = build(:task, content: nil, position: nil)
+      expect(task).not_to be_valid
+    end
+  end
 end
