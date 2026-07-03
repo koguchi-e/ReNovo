f68f7f7e3165442e14ac700dd59fba63303acde6
koguchi-e <koguchi.e915@gmail.com>
2026-07-03 16:18:37 +0900

    RubyLLMを導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,10 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
+  private
+
+  def available_chat_models
+    RubyLLM.models.chat_models.all
+           .sort_by { |model| [ model.provider.to_s, model.name.to_s ] }
+  end
 end
