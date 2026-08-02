d06f4c80d615cdf104342fb62b4435196891d355
koguchi-e <koguchi.e915@gmail.com>
2026-08-02 14:52:41 +0900

    フラッシュメッセージが消える際に予約を入れるよう修正

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   connect() {
-    setTimeout(() => {
+    this.dismissTimer = setTimeout(() => {
       this.element.remove();
     }, 2000);
   }
