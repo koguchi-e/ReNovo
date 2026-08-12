18e040f56e1ae96c560bf4e147b7e0b30c91d341
koguchi-e <koguchi.e915@gmail.com>
2026-08-12 17:37:38 +0900

    デザインレビューに向けallow_browser versions: :modernをコメントアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  allow_browser versions: :modern
+  # allow_browser versions: :modern
   stale_when_importmap_changes
 
   before_action :require_login
