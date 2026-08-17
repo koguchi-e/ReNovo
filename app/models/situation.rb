5fdde3241b26dcfdb3908c2460fedcc36895899a
koguchi-e <koguchi.e915@gmail.com>
2026-08-17 13:26:17 +0900

    pending状態で5分経過した場合、failedになるよう修正

--- app/models/situation.rb
+++ app/models/situation.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Situation < ApplicationRecord
+  GENERATION_TIMEOUT = 5.minutes
+
   belongs_to :user
   has_many :tasks, dependent: :destroy
 
@@ -14,4 +16,8 @@ class Situation < ApplicationRecord
     completed: 2,
     failed: 3
   }
+
+  def generation_timed_out?
+    pending? && created_at < GENERATION_TIMEOUT.ago
+  end
 end
